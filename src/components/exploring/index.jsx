4676ce9ed24f5ddce9996ecc0acfff4dafafa82a
TibaesDaniel <daniel.tibaes@yahoo.com>
2023-06-29 22:15:05 -0300

    responsive

--- src/components/exploring/index.jsx
+++ src/components/exploring/index.jsx
@@ -5,7 +5,7 @@ export default function exploring(){
         <div className='exploring content'>
             <div className=' boxExploring'>
                 <h1>EXPLORING NATURE</h1>
-                <p>When I explore nature, I like to use all my senses</p>
+                <p className='subtitleEx'>When I explore nature, I like to use all my senses</p>
                 <p className='boxText'>I like to see the colors, shapes, and patterns of the plants, animals, and landscapes. I like to hear the sounds, melodies, and rhythms of the wind, water, and wildlife. I like to smell the fragrances, aromas, and scents of the flowers, fruits, and herbs. I like to touch the textures, temperatures, and sensations of the soil, rocks, and leaves</p>
             </div>
         </div>
